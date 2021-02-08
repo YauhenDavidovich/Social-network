4ca4b86660470c714dbe5bf30c910be2a353ddff
Yauhen Davidovich <davidovich336@gmail.com>
2021-02-08 23:14:40 +0300

    feat: add basic markup

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,33 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div className='app-wrapper'>
+            <header className='header'>
+                <img src="https://www.onlinelogomaker.com/blog/wp-content/uploads/2017/07/door-company-logo.jpg"/>
+            </header>
+            <nav className='nav'>
+                <div>Profile</div>
+                <div>Messages</div>
+                <div>Music</div>
+            </nav>
+            <div className='content'>
+                <div><img
+                    src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJkHIXlWZwX8_MOjlmFPqY5kRRCayLbVVnqA&usqp=CAU'/>
+                </div>
+                <div>Ava+Description</div>
+                <div>My posts
+                    <div>New post</div>
+                    <div>
+                        <div>Post1</div>
+                        <div>Post2</div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    )
 }
 
 export default App;
