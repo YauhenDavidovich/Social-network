9e5376cea80c4f39cf1f8fe2b701e95a8f070e6d
Yauhen Davidovich <davidovich336@gmail.com>
2021-02-15 21:32:46 +0300

    feat: add dialogs navlinks

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import style from './Diaologs.module.css'
-
+import {NavLink} from 'react-router-dom'
 
 const Dialogs = () => {
     return (
         <div className={style.dialogs}>
             <div className={style.dialogs__items}>
-                <div className={style.dialog + ' ' + style.active}>Peter</div>
-                <div className={style.dialog}>Olga</div>
-                <div className={style.dialog}>Jack</div>
-                <div className={style.dialog}>Tomas</div>
+                <div className={style.dialog + ' ' + style.active}>
+                    <NavLink to='/dialogs/1'>Peter</NavLink>
+                </div>
+                <div className={style.dialog}>
+                    <NavLink to='/dialogs/2'>Olga</NavLink>
+                </div>
+                <div className={style.dialog}>
+                    <NavLink to='/dialogs/3'>Jack</NavLink>
+                </div>
+                <div className={style.dialog}>
+                    <NavLink to='/dialogs/4'>Tomas</NavLink>
+                </div>
+
             </div>
 
             <div className={style.messages}>
@@ -17,7 +26,6 @@ const Dialogs = () => {
                 <div className={style.message}>Yo!</div>
                 <div className={style.message}>Thanks!</div>
                 <div className={style.message}>Bye</div>
-
             </div>
         </div>
     )
