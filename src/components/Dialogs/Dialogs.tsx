a14533474c0e1f9043eb98fc1efef0e6ed08d78d
Yauhen Davidovich <davidovich336@gmail.com>
2021-02-15 21:12:02 +0300

    feat: add dialogs page

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -4,10 +4,24 @@ import style from './Diaologs.module.css'
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={style.dialogs}>
+            <div className={style.dialogs__items}>
+                <div className={style.dialog + ' ' + style.active}>Peter</div>
+                <div className={style.dialog}>Olga</div>
+                <div className={style.dialog}>Jack</div>
+                <div className={style.dialog}>Tomas</div>
+            </div>
+
+            <div className={style.messages}>
+                <div className={style.message}>Hi!</div>
+                <div className={style.message}>Yo!</div>
+                <div className={style.message}>Thanks!</div>
+                <div className={style.message}>Bye</div>
+
+            </div>
         </div>
-    );
-};
+    )
+
+}
 
 export default Dialogs;
