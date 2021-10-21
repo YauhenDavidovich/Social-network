d98060812c848dfbf59a5fe054c6c2f06a477c5b
Yauhen Davidovich <davidovich336@gmail.com>
2021-10-21 17:54:18 +0200

    feat: add gh-pages homepage

--- src/components/Profile/ProfileContainer.tsx
+++ src/components/Profile/ProfileContainer.tsx
@@ -1,7 +1,7 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import Profile from "./Profile";
 import {connect} from "react-redux";
-import {updateStatus, getStatus, getUserProfile, ProfileResponseType} from "../../redux/profile-reducer";
+import {getStatus, getUserProfile, ProfileResponseType, updateStatus} from "../../redux/profile-reducer";
 import {RouteComponentProps, withRouter} from "react-router-dom";
 import {withAuthRedirect} from "../../hoc/withAuthRedirect";
 import {compose} from "redux";
